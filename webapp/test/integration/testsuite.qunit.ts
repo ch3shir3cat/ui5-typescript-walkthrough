57be7993ce07877667b0557e84b4bf8b4771785f
Dwarakanath Maligi <dwarakanath@maligi.com>
2025-09-02 18:14:34 -0400

    Step 28: Integration Test with OPA (TypeScript)

---
+++ webapp/test/integration/testsuite.qunit.ts
@@ -0,0 +1,11 @@
+export default {
+	  // ...
+	  tests: {
+		    "unit/unitTests": {
+			       title: "UI5 TypeScript Walkthrough - Unit Tests"
+		    },
+		    "integration/opaTests": {
+			       title: "UI5 TypeScript Walkthrough - Integration Tests"
+		    }
+	  }
+};
